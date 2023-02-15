f6a386646a3e4cd93f69129a8766702202d51b9e
JasubiPL <gameportal7@hotmail.com>
2023-02-15 12:44:15 -0600

    Se añade Next UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@nextui-org/react';
+
 export default function Home() {
   return (
     <>
       <h1>Hola Mundo</h1>
+      <Button color={'gradient'}>Holis</Button>
     </>
   )
 }
