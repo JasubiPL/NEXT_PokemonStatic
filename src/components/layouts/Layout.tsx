893d3b95a70ffc430f6ecf24bcf0514ce751603b
JasubiPL <gameportal7@hotmail.com>
2023-03-09 11:49:15 -0600

    Se añadio Nav

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react'
 import Head from "next/head";
+import { Navbar } from '../ui';
 
 interface Props{
   title?:string;
@@ -18,9 +19,11 @@ export const Layout: FC<Props> = ({children, title, pokemonName}) => {
         <meta name="keywords" content={`${title}, pokemon, pokedex`} />
       </Head>
 
-      {/*NavBar*/}
+      <Navbar />
 
-      <main>
+      <main style={{
+        padding: '0px 20px'
+      }}>
         {children}
       </main>
     </>
